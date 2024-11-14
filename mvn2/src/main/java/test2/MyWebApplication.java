6fe56ef02da7bc8b11cea50705de765ab9078c22
sarath <sscv1996@gmail.com>
2024-11-14 13:13:14 +0530

    ats task

---
+++ mvn2/src/main/java/test2/MyWebApplication.java
@@ -0,0 +1,16 @@
+package test2;
+
+import org.orienteer.core.OrienteerWebApplication;
+import org.orienteer.core.module.PerspectivesModule;
+
+public class MyWebApplication extends OrienteerWebApplication
+{
+	@Override
+	public void init()
+	{
+		super.init();
+		mountPackage("test2.web");
+		registerModule(DataModel.class);
+	}
+	
+}
